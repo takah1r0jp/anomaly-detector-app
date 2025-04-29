c4b9bbf348b92120c0e7bf6be5625691277832c5
Takahiro Tsurumaki <tsurumaki.takahiro.r1@dc.tohoku.ac.jp>
2025-04-29 22:58:20 +0900

    Add condition box

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const conditionRef = useRef();
+  const [normalcondition, setCondition] = useState(["There are two apples."]);
+
+  const handleAddCondition = () => {
+    const condition = conditionRef.current.value;
+    console.log(condition)
+    if (condition) {
+      setCondition([...normalcondition, condition]);
+
+      conditionRef.current.value = null;
+    }
+  };
+  
   return (
     <div className="app">
       {/* ヘッダー */}
@@ -14,8 +27,27 @@ function App() {
       {/* 入力エリア */}
       <section className="input-section">
 
-        <label htmlFor="normal">正常条件を入力して</label>
-        <input type="text" id="normal" placeholder="入力してください" />
+        <label htmlFor="normal">正常条件を入力してください</label>
+
+        <div className="input-with-button">
+          <input type="text" id="normal" placeholder="入力してください" ref={conditionRef}  />
+          <button onClick={handleAddCondition}>追加</button>
+        </div>
+
+        {/* 追加された正常条件の表示 */}
+        <div className="condition-list">
+          <h3>追加された正常条件：</h3>
+          {normalcondition.length > 0 ? (
+            <ul>
+              {normalcondition.map((condition, index) => (
+                <li key={index}>{condition}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>まだ条件が追加されていません</p>
+          )
+          } 
+        </div>
 
         <label htmlFor="image">異常検知画像</label>
         <div className="image-box">ここに画像</div>
